511fa56646926a4b36abdecd60767935629c4afd
Nandete123 <nandete1@gmail.com>
2023-11-08 20:42:33 +0100

    Modelo casi finiquitado

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
 import pandas as pd
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 
 
 titanic_training = pd.read_csv(r"C:\Users\BigData\Desktop\CursoBigData\csvTitanic\train.csv")
@@ -17,8 +19,7 @@ X["Q"] = X_transformada_Embark["Q"]
 X["S"] = X_transformada_Embark["S"]
 X["male"] = X_transformada_Sex["male"]
 X["female"] = X_transformada_Sex["female"]
-survived = titanic_sinNan.drop(columns=["Name", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked", "Pclass", "PassengerId"])
-y = survived
+y = titanic_sinNan["Survived"].to_numpy()
 
 print(X)
 print(y)
@@ -26,8 +27,30 @@ print(y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=16)
 
 
-lir = LogisticRegression()
+lir = LogisticRegression(max_iter=500)
+ada = AdaBoostClassifier(n_estimators=150, random_state = SEED)
+bagg = BaggingClassifier(estimator=SVC(), n_estimators=25, random_state=0).fit(X, y)
+rf = RandomForestClassifier(max_depth=3, random_state=0)
 
+rf.fit(X_train, y_train)
+print("Random Forest Classifier: {:.3f}" .format(rf.score(X_test, y_test)))
 lir.fit(X_train, y_train)
-print("Linear Regression: {:.3f}" .format(lir.score(X_test, y_test)))
+print("Logistic Classifier: {:.3f}" .format(lir.score(X_test, y_test)))
+ada.fit(X_train, y_train)
+print("Ada Boost Regressor: {:.3f}" .format(ada.score(X_test, y_test)))
+bagg.fit(X_train, y_train)
+print("Bagging Regressor: {:.3f}" .format(bagg.score(X_test, y_test)))
+
+classifiers = [('Logistic Classifier', lir),
+               ('Random Forest Regressor', rf),
+               ('Ada Boost Classifier', ada)]
+
+for clf_name, clf in classifiers:
+  clf.fit(X_train, y_train)
+  #print("{:s}: {:.3f}" .format(clf_name, clf.score(X_test, y_test)))
+
+vc = VotingClassifier(estimators=classifiers)
+vc.fit(X_train, y_train)
+print("Voting Classifier: {:.3f}" .format(vc.score(X_test, y_test)))
+
 
