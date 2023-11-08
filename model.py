995ff46cda9f8c19970a749e07ed9f93e4f8f9a1
Nandete123 <nandete1@gmail.com>
2023-11-08 20:19:22 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,33 @@
-import pandas as pd;
+import pandas as pd
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LogisticRegression
 
 
-train=pd.read_csv('train.csv')
-print(train)
+
+titanic_training = pd.read_csv(r"C:\Users\BigData\Desktop\CursoBigData\csvTitanic\train.csv")
+titanic_test = pd.read_csv(r"C:\Users\BigData\Desktop\CursoBigData\csvTitanic\test.csv")
+titanic_sinNan = titanic_training.dropna()
+
+SEED = 42
+X_transformada_Sex = pd.get_dummies(titanic_sinNan.Sex)
+X_transformada_Embark = pd.get_dummies(titanic_sinNan.Embarked)
+X = titanic_sinNan.drop(columns=["Name","Sex","Ticket", "Cabin", "Embarked", "Survived"])
+X["C"] = X_transformada_Embark["C"]
+X["Q"] = X_transformada_Embark["Q"]
+X["S"] = X_transformada_Embark["S"]
+X["male"] = X_transformada_Sex["male"]
+X["female"] = X_transformada_Sex["female"]
+survived = titanic_sinNan.drop(columns=["Name", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked", "Pclass", "PassengerId"])
+y = survived
+
+print(X)
+print(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=16)
+
+
+lir = LogisticRegression()
+
+lir.fit(X_train, y_train)
+print("Linear Regression: {:.3f}" .format(lir.score(X_test, y_test)))
+
