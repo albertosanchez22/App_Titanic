097427e48db1efc0719882d7ebf79e9d18e356b5
albertosanchez22 <alberto.sanchez@iesdoctorbalmis.com>
2023-11-15 20:43:46 +0100

    terminado

--- app.py
+++ app.py
@@ -11,23 +11,35 @@ def home():
     return render_template('template.html')
 
 
-#@app.route('/predict',methods=['POST'])
-#def predict():
+@app.route('/predict',methods=['POST'])
+def predict():
 
-#    int_features = [int(x) for x in request.form.values()]
- #   final_features = [np.array(int_features)]
-   
+    int_features = [int(x) for x in request.form.values()]
+    print("FEATURES",request.form.values())
+    final_features = [np.array(int_features)]
+    print("FINAL",final_features)
+    prediction = model.predict(final_features)
+    estado=""
+    if(prediction==0):
+        estado="Muerto"
+    else:
+        estado="Vivito y coleando"
 
-  #  return render_template('template.html', prediction_text=final_features)
+    return render_template('template.html', prediction_text='El pasajero está: {}'.format(estado))
 
 @app.route('/results',methods=['POST'])
 def results():
     data = request.get_json(force=True)
+    print("DATA",data)
     prediction = model.predict([np.array(list(data.values()))])
-
+    print("PREDICTION",prediction)
     output = prediction[0]
+    print("OUTPUT",output)
+    if(output==0):
+        output="HA MUERTO"
+    else:
+        output="NO HA MUERTO"
     return jsonify(output)
-
 
 
 if __name__ == "__main__":
--- request.py
+++ request.py
@@ -1,6 +1,5 @@
 import requests
 url = 'http://localhost:5000/results'
-r = requests.post(url,json={"PassengerId":20,"Pclass":1,"Age":93.0,"SibSp":1,"Parch":0,"Fare":12.0000,"C":1,"Q":0,"S":0,"male":1,"female":0})
-#r = requests.post(url)
+r = requests.post(url,json={"PassengerId":20,"Pclass":1,"Age":25,"SibSp":1,"Parch":0,"Fare":12,"C":1,"Q":0,"S":0,"male":0,"female":1})
 print("R= ",r)
 print(r.json())
